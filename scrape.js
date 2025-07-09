0163dee05429da322bbb140fd185c1e1a27f0de2
sunnytsai <sunnytsai@christiedigital.com>
2025-07-09 16:13:29 -0400

    update

--- scrape.js
+++ scrape.js
@@ -10,41 +10,41 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
 
   const routesToScrape = [
-    '/#/status',  // Status Page
-    '/#/status/AllAlarms',
-    '/#/status/Versions',
-    '/#/status/Fans',
-    '/#/status/Temperatures',
-    '/#/status/System',
-    '/#/status/Lamp',
-    '/#/status/Lens',
-    '/#/status/Network',
-    '/#/status/Interlocks',
-    '/#/status/Serial',
-    '/#/status/Video',
-    '/#/status/Playback',
-    '/#/status/Scheduler',
-    '/#/status/Automation',
-    '/#/status/ChristieNAS',
-    '/#/status/Debugging',
+    // '/#/status',  // Status Page
+    // '/#/status/AllAlarms',
+    // '/#/status/Versions',
+    // '/#/status/Fans',
+    // '/#/status/Temperatures',
+    // '/#/status/System',
+    // '/#/status/Lamp',
+    // '/#/status/Lens',
+    // '/#/status/Network',
+    // '/#/status/Interlocks',
+    // '/#/status/Serial',
+    // '/#/status/Video',
+    // '/#/status/Playback',
+    // '/#/status/Scheduler',
+    // '/#/status/Automation',
+    // '/#/status/ChristieNAS',
+    // '/#/status/Debugging',
     '/#/menu/MMM%2BABOT',  // About Page
-    '/#/menu/MMM%2BSERV/MMM%2BPREF/General',
-    '/#/menu/MMM%2BSERV/MMM%2BPREF/HDR',
-    '/#/menu/MMM%2BSERV/MMM%2BPREF/Alarm%20Triggers',
-    '/#/menu/MMM%2BSERV/MMM%2BPREF/Service%20Mode',
-    '/#/menu/MMM%2BSERV/MMM%2BPREF/Lens%20ILS',
-    '/#/menu/MMM%2BSERV/MMM%2BPREF/Automation',
-    '/#/menu/MMM%2BSERV/MMM%2BPREF/IMB',
-    '/#/menu/MMM%2BSERV/MMM%2BPREF/3D%20Configuration',
-    '/#/menu/MMM%2BSERV/MMM%2BPREF/Test%20Patterns',
-    '/#/menu/MMM%2BSERV/MMM%2BPREF/EDID%20File%20Import',
-    '/#/menu/MMM%2BAUTO/MMM%2BMCRE',    
-    '/#/menu/MMM%2BAUTO/MMM%2BDIPT/PLTR',
-    '/#/menu/MMM%2BAUTO/MMM%2BAUDD',
-    '/#/menu/MMM%2BAUTO/MMM%2BDLIB',
-    '/#/menu/MMM%2BCHAN',
-    '/#/menu/MMM%2BIMGS/MMM%2BILSF',
-    '/#/dashboard', //dashboard
+    // '/#/menu/MMM%2BSERV/MMM%2BPREF/General',
+    // '/#/menu/MMM%2BSERV/MMM%2BPREF/HDR',
+    // '/#/menu/MMM%2BSERV/MMM%2BPREF/Alarm%20Triggers',
+    // '/#/menu/MMM%2BSERV/MMM%2BPREF/Service%20Mode',
+    // '/#/menu/MMM%2BSERV/MMM%2BPREF/Lens%20ILS',
+    // '/#/menu/MMM%2BSERV/MMM%2BPREF/Automation',
+    // '/#/menu/MMM%2BSERV/MMM%2BPREF/IMB',
+    // '/#/menu/MMM%2BSERV/MMM%2BPREF/3D%20Configuration',
+    // '/#/menu/MMM%2BSERV/MMM%2BPREF/Test%20Patterns',
+    // '/#/menu/MMM%2BSERV/MMM%2BPREF/EDID%20File%20Import',
+    // '/#/menu/MMM%2BAUTO/MMM%2BMCRE',    
+    // '/#/menu/MMM%2BAUTO/MMM%2BDIPT/PLTR',
+    // '/#/menu/MMM%2BAUTO/MMM%2BAUDD',
+    // '/#/menu/MMM%2BAUTO/MMM%2BDLIB',
+    // '/#/menu/MMM%2BCHAN',
+    // '/#/menu/MMM%2BIMGS/MMM%2BILSF',
+    // '/#/dashboard', //dashboard
     
   ];
 
@@ -61,9 +61,20 @@ const puppeteer = require('puppeteer');
     await new Promise(resolve => setTimeout(resolve, 2000));
 
     const content = await page.evaluate(() => {
-      return Array.from(document.querySelectorAll('body *' || 'body #info-display' || 'body #copyright-text'))
-        .map(el => el.innerText.trim())
-        .filter(text => text.length > 0 );
+      var all = document.querySelector("#info-display");
+      console.log(all);
+      // create range [0,1,2,....,all.length-1]
+      // Array.from({length: all.length}, (v, k) => k).forEach((index) => {
+      //   let element = all.item(index);
+      //   console.log('index:',index,',element:',all.item(index));
+      // })
+      //return Array.from(document.querySelectorAll('body *' || 'body #info-display' || 'body #copyright-text'))
+      return Array.from(document.querySelector("body #info-display"))
+     // .map(el => el.innerText.trim())
+     // .filter(text => text.length > 0 );
+      //return Array.from(document.querySelectorAll('body *,#info-display,#copyright-text')).
+      //  .map(el => el.innerText.trim())
+      //  .filter(text => text.length > 0 );
     });
 
     allResults.push({
